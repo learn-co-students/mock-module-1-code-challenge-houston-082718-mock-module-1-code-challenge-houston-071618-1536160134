76ac82a638581aaee01d44895ad44534de200c69
github username <github email address>
2018-09-05 11:13:01 -0500

    done

--- book.rb
+++ book.rb
@@ -1,1 +1,44 @@
+class Book
+@@all = []
 
+  attr_reader :author
+  attr_accessor :title, :word_count
+
+def initialize(title, word_count, author)
+ @title = title
+ @word_count = word_count
+ @author = author
+ @@all << self
+end
+
+def self.all
+  @@all
+end
+ #
+ # def author
+ #   Author.all.find{|author|
+ #   author.book == self }
+ # end
+
+ # def title
+ #
+ # end
+ #
+ # def word_count
+ #
+ # end
+
+# moby_dick = Book.new("moby dick" 585, "Herman melville")
+# to_kill_a_mockingbird = Book.new("To kill a Mockingbird", 281, "Harper lee")
+
+end
+
+
+# - `Book.all`
+# should return all of the books
+# - `Book#author`
+# should return the author instance who wrote this book
+# - `Book#title`
+# should return the title of the book
+# - `Book#word_count`
+# should return the number of words in the book
--- console.rb
+++ console.rb
@@ -2,6 +2,16 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+herman_melville = Author.new("Herman Melville")
+harper_lee = Author.new("Harper_lee")
+richard_ojo = Author.new("Richard Ojo")
+bami_dele = Author.new("Bamidele olaitan")
+
+moby_dick = Book.new("moby dick", 585, herman_melville)
+to_kill_a_mockingbird = Book.new("To kill a Mockingbird", 281, harper_lee)
+basketball_and_coding = Book.new("basketballand coding", 500, richard_ojo)
+the_laundryman = Book.new("The laundryman", 2000, bami_dele)
 binding.pry
 
-
+true
