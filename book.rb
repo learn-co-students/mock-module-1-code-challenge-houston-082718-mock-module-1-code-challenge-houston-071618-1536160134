e701faef6a6933782c43469b4ae4827b760f25b3
gwatson86 <gwatson86@gmail.com>
2018-09-05 10:46:34 -0500

    done

--- book.rb
+++ book.rb
@@ -1,1 +1,30 @@
+class Book
 
+    attr_reader :title, :author, :word_count
+
+    @@all = []
+
+    def initialize(title, author, word_count)
+        @title = title
+        @author = author
+        @word_count = word_count
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author
+        @author
+    end
+
+    def title
+        @title
+    end
+
+    def word_count
+        @word_count
+    end
+
+end
