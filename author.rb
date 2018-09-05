3ed4e81930dd5fe3e49c88f1297d6f5bc01b5f01
EthanFe <sneakcow@gmail.com>
2018-09-05 10:28:15 -0500

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,31 @@
+class Author
+  @@all =[]
 
+  def initialize
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select {|book| book.author == self}
+  end
+
+  def write_book(title, word_count)
+    Book.new(self, title, word_count)
+  end
+
+  def total_words
+    total = 0
+    self.books.each do |book|
+      total += book.word_count
+    end
+    total
+  end
+
+  def self.most_words
+    self.all.sort_by {|author| author.total_words}.last
+  end
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,17 @@
+class Book
+  @@all = []
 
+  attr_reader :author, :title, :word_count
+
+  def self.all
+    @@all
+  end
+
+  def initialize(author, title, word_count)
+    @author = author
+    @title = title
+    @word_count = word_count
+
+    self.class.all << self
+  end
+end
