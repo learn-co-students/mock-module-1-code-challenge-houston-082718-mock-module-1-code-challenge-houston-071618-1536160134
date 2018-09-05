01794fbbded5407e4255477f975fdbbf1a888b5d
Flatiron School <andrewsprague@outlook.com>
2018-09-05 10:59:24 -0500

    done

--- book.rb
+++ book.rb
@@ -1,1 +1,21 @@
+class Book
 
+    attr_reader :author, :title
+    attr_accessor :word_count
+
+    @@all = []
+
+    def initialize(author, title, word_count)
+        @author = author
+        @title = title
+        @word_count = word_count
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,33 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+jkrowling = Author.new("J.K. Rowling")
+grrmartin = Author.new("George R.R. Martin")
+
+hpss = jkrowling.write_book("Harry Potter and the Sorcerer's Stone", 309)
+jkrowling.write_book("Harry Potter and the Chamber of Secrets", 341)
+jkrowling.write_book("Harry Potter and the Prisoner of Azkaban", 435)
+jkrowling.write_book("Harry Potter and the Goblet of Fire", 734)
+jkrowling.write_book("Harry Potter and the Order of the Phoenix", 870)
+jkrowling.write_book("Harry Potter and the Half-Blood Prince", 652)
+jkrowling.write_book("Harry Potter and the Deathly Hallows", 759)
+
+
+
+grrmartin.write_book("A Game of Thrones", 694)
+grrmartin.write_book("A Clash of Kings", 768)
+grrmartin.write_book("A Storm of Swords", 973)
+# grrmartin.write_book("A Dance with Dragons", 1040)
+
+#jrrtolkien = Author.new("John Ronald Reuel Tolkien")
+
+#jrrtolkien.write_book("The Fellowship of the Ring", 423)
+
+
+
+
+
+
 binding.pry
 
 
