698c87778db991fa0d917a65838683ba4006a957
chelsme <chelsea.crowson@me.com>
2018-09-05 11:11:31 -0500

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,44 @@
+class Author
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def write_book(title, word_count)
+        book = Book.new(title, self, word_count)
+    end
+
+    def books
+        books = Book.all.select do |book|
+            book.author == self
+        end
+        books.map do |book|
+            book.title
+        end
+    end
+
+    def total_words
+        books = Book.all.select do |book|
+            book.author == self
+        end
+        words = books.map do |book|
+            book.word_count
+        end.flatten
+        sum = words.reduce :+
+    end
+
+    def self.most_words
+        words = self.all.map do |author|
+            author.total_words
+        end
+        words.max
+    end
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,13 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+lois_lowry = Author.new("Lois Lowry")
+the_giver = Book.new("The Giver", lois_lowry, 312000)
+margaret_atwood = Author.new("Margaret Atwood")
+the_handmaids_tale = Book.new("The Handmaid's Tale", margaret_atwood, 459000)
+alias_grace = Book.new("Alias Grace", margaret_atwood, 59483)
+maddaddam = Book.new("MaddAddam", margaret_atwood, 98573)
+
 binding.pry
 
 
