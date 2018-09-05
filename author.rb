28e3459bed2805f049a55048048f65da11edaa7b
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-05 11:10:40 -0500

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,41 @@
+# Author.all should return all of the authors
+# Author#books should return all of the books an author has written
+# Author#write_book should take arguments of a title and word count and make a new Book instance associated with this author
+# Author#total_words should return the total number of words that author has written across all of their authored books.
+# Author.most_words should return the author instance who has written the most words
 
+class Author
+  attr_accessor :name
+  @@author_total_words = {}
+  def initialize(name)
+    @name = name
+  end
+
+  def books
+    Book.all.select {|book| book.author == self.name}
+  end
+
+  def write_book(title,word_count)
+    new_book = Book.new(title,self.name,word_count)
+  end
+
+  def total_words
+    total = 0
+    self.books.each do |books|
+      total +=books.word_count
+    end
+    @@author_total_words = {self: total}
+    total
+  end
+
+  def self.most_words
+    most_words = 0
+    author_most_words = Author.new("Replace me with author with most words")
+    @@author_total_words.each do |author,total_words|
+      if total_words > most_words
+        @@author_total_words[author]
+      end
+    end
+  end
+
+end
--- console.rb
+++ console.rb
@@ -1,7 +1,14 @@
 require 'pry'
-require_relative './book'
-require_relative './author'
+require_relative './book.rb'
+require_relative './author.rb'
 
-binding.pry
+book_1 = Book.new("Book title A1", "AuthorA", 1000)
+book_2 = Book.new("Book title A2", "AuthorA", 2000)
+book_3 = Book.new("Book title B1","AuthorB", 1500)
+book_4 = Book.new("Book title B2","AuthorB", 2500)
+author_1 = Author.new("AuthorA")
+author_2 = Author.new("AuthorB")
+
+#binding.pry
 
 
